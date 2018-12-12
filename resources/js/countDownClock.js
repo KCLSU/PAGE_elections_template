2f6c7c145ed4b538f1a4e2c0b2604a589ef204ed
Kyle Robinson <KyleR@kclsu.org>
2018-12-12 17:40:56 +0000

    countdown functionality for each stage

--- resources/js/countDownClock.js
+++ resources/js/countDownClock.js
@@ -1,28 +1,106 @@
 $(document).ready(function(){
-const countDownDate = new Date('January 28, 2019, 10:00:00').getTime();
-const timer = setInterval(function(){
+  let countDownDate = new Date('January 28, 2019, 10:00:00').getTime();
 
-  const timeNow = new Date().getTime();
-  const distance = countDownDate - timeNow;
+  let timeNow = new Date().getTime();
+  let days = null;
+  let hours = null;
+  let minutes = null;
+  let seconds = null;
+  let distance = null;
+console.log("top level distance is: " + distance)
+
+function calculateDistance(){
+    console.log("distance triggered")
+  timeNow = new Date().getTime();
+  distance = countDownDate - timeNow;
+  console.log("distance is NOW: " + distance)
+  return distance;
+}
+
 
     //CONVERT TIME CALCS INTO DAYS, HOURS, MINS, SECS
+function calculateTimes(x){
+  console.log("calculateTimes triggered")
+    days = Math.floor(x / (1000 * 60 * 60 * 24));
+    hours = Math.floor((x % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    minutes = Math.floor((x % (1000 * 60 * 60)) / (1000 * 60));
+    seconds = Math.floor((x % (1000 * 60)) / 1000);
+  }
 
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    //DISPLAY RESULT IN HTML
-      document.getElementById('countdown').innerHTML = `${days}:${hours}:${minutes}:${seconds}`;
+function startTimer(){
+  const timer1 = setInterval(function(){
+    console.log("first timer")
+    calculateTimes(calculateDistance())
+    // console.log(`${days}:${hours}:${minutes}:${seconds}`)
 
     //WHEN THE COUNTDOWN FINISHES
-
     if (distance < 0) {
-      clearInterval(timer);
-      document.getElementById('countdown').innerHTML = 'Nominations are open!';
+      clearInterval(timer1);
+      nominationsClose()
     }
 
+    //DISPLAY RESULT IN HTML
+    document.getElementById('countdown').innerHTML = `${days}:${hours}:${minutes}:${seconds}`;
 }, 1000);
+
+function nominationsClose(){
+  countDownDate = new Date('January 29, 2019, 10:00:00').getTime();
+  const timer2 = setInterval(function(){
+      console.log("second timer")
+      calculateTimes(calculateDistance())
+
+      //WHEN THE COUNTDOWN FINISHES
+      if (distance < 0) {
+        clearInterval(timer2);
+        candidatesAnnounced()
+      }
+      //DISPLAY RESULT IN HTML
+      document.getElementById('countdown').innerHTML = `${days}:${hours}:${minutes}:${seconds}`;
+  }, 1000);
+}
+
+function candidatesAnnounced(){
+  countDownDate = new Date('February 15, 2019, 12:00:00').getTime();
+
+  const timer3 = setInterval(function(){
+      console.log("Third timer")
+    calculateTimes(calculateDistance())
+
+    //WHEN THE COUNTDOWN FINISHES
+    if (distance < 0) {
+      clearInterval(timer3);
+      votingOpens()
+    }
+
+    //DISPLAY RESULT IN HTML
+    document.getElementById('countdown').innerHTML = `${days}:${hours}:${minutes}:${seconds}`;
+    }, 1000);
+
+}
+
+function votingOpens(){
+  countDownDate = new Date('February 25, 2019, 12:00:00').getTime();
+  const timer3 = setInterval(function(){
+    calculateTimes(calculateDistance())
+      //WHEN THE COUNTDOWN FINISHES
+
+      if (distance < 0) {
+        clearInterval(timer3);
+        // countDownDate = new Date('March 4, 2019, 10:00:00').getTime();
+        // document.getElementById('countdown').innerHTML = 'Nominations are open!';
+        votingCloses()
+      }
+
+      //DISPLAY RESULT IN HTML
+      document.getElementById('countdown').innerHTML = `${days}:${hours}:${minutes}:${seconds}`;
+  }, 1000);
+}
+
+}
+
+startTimer();
+
 
 
 
