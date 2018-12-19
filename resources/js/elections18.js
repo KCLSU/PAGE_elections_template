4d6cf8477a80386e6a859c1f526e5bbeb9df2903
Kyle Robinson <KyleR@kclsu.org>
2018-12-19 10:29:47 +0000

    msl first commit

--- resources/js/elections18.js
+++ resources/js/elections18.js
@@ -1,8 +1,17 @@
 
+//THIS IS THE COMPILED JS FOR UPLOADING TO MSL
+//SEE KCLSU GITHUB REPO
+
+
 document.getElementById("default-tab").click();
 document.getElementById("default-mini-tab").click();
 document.getElementById('burger-content').style.display = 'none';
 
+// MOBILE BURGER FUNCTION
+function burgerMenu(id){
+  console.log("open/close menu")
+  $(id).slideToggle("slow");
+}
 
 function closeBurger(){
   $('#burger-content').slideUp('slow');
@@ -13,7 +22,7 @@ var tabButtons = document.querySelectorAll('.tab-button')
 
 document.getElementById("default-tab").click();
 
-
+ // MAIN TAB FUNCTIONS FOR PRIMARY TABS
 function openTab(event, id){
 
   let targetSection = document.getElementById(id);
@@ -33,14 +42,19 @@ function openTab(event, id){
 }
 
 
-var miniTabSections = document.querySelectorAll('.mini-tab-section')
-var miniTabButtons = document.querySelectorAll('.mini-tab')
-const tabDefault = document.getElementById('default-mini-tab');
-miniTabSections.forEach(section => {
+// MINI TAB FUNCTIONS FOR TABS WITHIN TABS
+var miniTabSectionsTemp = document.querySelectorAll('.mini-tab-section');
+// var miniTabButtons = document.querySelectorAll('.mini-tab')
+// const tabDefault = document.getElementById('default-mini-tab');
+miniTabSectionsTemp.forEach(section => {
   section.style.display = 'none';
 })
 
-function openMiniTab(event, id){
+function openMiniTab(event, id, defaultName, buttonsName, sectionsName){
+  let miniTabSections = document.querySelectorAll(`.${sectionsName}`)
+  let miniTabButtons = document.querySelectorAll(`.${buttonsName}`)
+  const tabDefault = document.getElementById(`${defaultName}`);
+
   tabDefault.style.display = 'none'
   let targetSection = document.getElementById(id);
   console.log("clicked")
@@ -54,10 +68,11 @@ function openMiniTab(event, id){
   })
 
   targetSection.style.display = 'block';
-event.target.classList.add('mini-tab-active');
+  event.target.classList.add('mini-tab-active');
   // event.target.classList.add('active');
 }
 
+// FUNCTION TO OPEN NOMINATE SECTION FROM WITHIN A TAB
 function nominate() {
  document.getElementById('nominate').click();
 }
